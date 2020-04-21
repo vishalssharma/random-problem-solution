190c2df592e2a3f41e8825c9ddc3d38c29302d57
Vishal <vishalsharma.vs@hotmail.com>
2020-04-21 17:57:59 +0530

    top five words function add

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,8 @@
 #include<chrono>
 using namespace std;
 using namespace std::chrono;
+int lb = 0;
+int ub = 4999;
 struct dictionary{
     string word;
     unsigned int freq;
@@ -33,13 +35,78 @@ void insertWords(){
     }
 
 
-    for(int j=0;j<=i;j++){
-        cout<<dict[j].word<<" "<<dict[j].freq;
-        cout<<endl;
-    }
+    // for(int j=0;j<=i;j++){
+    //     cout<<dict[j].word<<" "<<dict[j].freq;
+    //     cout<<endl;
+    // }
 
 }
-bool printTopWords(string s){
+void printTopWords(){
+    int left = lb;
+    int right = ub;
+    map<int,string> m;
+    for(int i=lb;i<ub;i++)
+    m[dict[i].freq] = dict[i].word;
+
+    map<int,string>::iterator itr;
+    itr = m.begin();
+    int i =0;
+    while(itr !=m.end() && i<5){
+        cout<<itr->second<<'\t';
+        i++;
+        itr++;
+    }
+}
+
+bool findTopWords(string str){
+    int lastIndex = str.size() -1;
+    char c = str[lastIndex];
+    int left = lb;
+    int right = ub-1;
+    int posMiddle = -1;
+    // binary search to find all matching words 
+    while(left<=right){
+        int mid = left + (right-left)/2;
+        if(dict[mid].word[lastIndex] == c){
+            posMiddle = mid;
+            break; 
+        }
+        else if(dict[mid].word[lastIndex] > c){
+            right = mid - 1;
+
+        }
+        else{
+            left = mid + 1;
+        }
+        
+    }
+    if( posMiddle == -1)
+        return false;
+    int firstOccurance = posMiddle;
+    while(dict[firstOccurance--].word[lastIndex] == c){}
+    lb = firstOccurance+1;
+    int lastOccurance = posMiddle;
+    while(dict[lastOccurance++].word[lastIndex] == c){}
+    ub = lastOccurance;
+
+    if((ub - lb) == 1){
+        cout<<dict[lb].word<<'\t';
+    }
+    else if((ub - lb) == 2){
+        if(dict[lb].freq >= dict[lb+1].freq){
+            cout<<dict[lb].word<<'\t';
+            cout<<dict[lb+1].word<<'\t';
+            
+        }
+        else{
+            cout<<dict[lb+1].word<<'\t';
+            cout<<dict[lb].word<<'\t';
+        }
+    }
+    else
+        printTopWords();
+    
+
     return true;
 
 }
@@ -51,7 +118,7 @@ int main(){
     while(c != '#'){
         s += c;
         auto start = high_resolution_clock::now();
-        bool flag = printTopWords(s);
+        bool flag = findTopWords(s);
         auto stop = high_resolution_clock::now();
         auto duration = duration_cast<microseconds>(stop-start);
         if(flag == false){
